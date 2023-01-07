f6071ec5880be64edef403037b2ed70d70041e79
Trycondius <adesousasalazar@gmail.com>
2023-01-07 15:09:42 +0000

    string message changed

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+# function that returns a string
+def print_string():
+    return "Git is Awesome!"
+
+
+# main of the program calling the above functions/methods, and prints the returned string
+print(print_string())
